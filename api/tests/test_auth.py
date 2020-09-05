47f6a587948329ad41327de98eb86cb9a3d42dae
Ian Beck <ian@onecrayon.com>
2020-09-04 23:17:49 -0700

    Added missing authentication tests for failure states

--- api/tests/test_auth.py
+++ api/tests/test_auth.py
@@ -6,8 +6,10 @@ from typing import Tuple
 from fastapi import status
 from fastapi.testclient import TestClient
 from freezegun import freeze_time
+from jose import jwt
 
 from api import db
+from api.environment import settings
 from .utils import _create_user_password, generate_random_chars, _create_user_token
 
 
@@ -56,7 +58,13 @@ def test_login_required(client: TestClient, session: db.Session):
     assert response.status_code == status.HTTP_200_OK, response.json()
 
 
-def test_login_required_invalid_token(client: TestClient, session: db.Session):
+def test_login_required_no_token(client: TestClient, session: db.Session):
+    """login_required does in fact require a login"""
+    response = client.get("/v2/players/me")
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED, response.json()
+
+
+def test_login_required_old_token(client: TestClient, session: db.Session):
     """login_required dependency requires a current token"""
     user, token = _create_user_token(session)
     tomorrow = datetime.utcnow() + timedelta(days=1)
@@ -64,7 +72,29 @@ def test_login_required_invalid_token(client: TestClient, session: db.Session):
         response = client.get(
             "/v2/players/me", headers={"Authorization": f"Bearer {token}"}
         )
-        assert response.status_code == 401, response.json()
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED, response.json()
+
+
+def test_login_required_missing_sub(client: TestClient, session: db.Session):
+    """login_required dependency requires the user badge in the `sub`"""
+    expire = datetime.utcnow() + timedelta(minutes=15)
+    fake_data = {"exp": expire}
+    fake_token = jwt.encode(fake_data, settings.secret_key)
+    response = client.get(
+        "/v2/players/me", headers={"Authorization": f"Bearer {fake_token}"}
+    )
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED, response.json()
+
+
+def test_login_required_no_such_badge(client: TestClient, session: db.Session):
+    """login_required dependency can handled badges that don't exist"""
+    expire = datetime.utcnow() + timedelta(minutes=15)
+    fake_data = {"exp": expire, "sub": "nopers"}
+    fake_token = jwt.encode(fake_data, settings.secret_key)
+    response = client.get(
+        "/v2/players/me", headers={"Authorization": f"Bearer {fake_token}"}
+    )
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED, resonse.json()
 
 
 def test_login_required_banned_user(client: TestClient, session: db.Session):
