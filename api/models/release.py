c7528c684ab6d6e8095b32a339efb790d759f9af
Ian Beck <ian@onecrayon.com>
2020-09-30 21:33:19 -0700

    Added `Release.stub` and `Release.is_public` properties
    Also fixed a method name mistake in the stubify helper and added an init method for Release to automatically create stubs, if necessary.

--- api/models/release.py
+++ api/models/release.py
@@ -1,18 +1,29 @@
 from api import db
+from api.utils.helpers import stubify
 from .user import User
 
 
 class Release(db.AlchemyBase):
     __tablename__ = "releases"
+    __table_args__ = (db.UniqueConstraint("stub", "is_legacy"),)
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(60), nullable=False, unique=True)
+    name = db.Column(db.String(60), nullable=False)
+    stub = db.Column(db.String(60), nullable=False)
+    is_legacy = db.Column(db.Boolean, nullable=False, default=False, index=True)
+    is_public = db.Column(db.Boolean, nullable=False, default=False, index=True)
+    # These fields are only used by legacy cards
     is_phg = db.Column(db.Boolean, nullable=False, default=False)
     is_promo = db.Column(db.Boolean, nullable=False, default=False)
     is_retiring = db.Column(db.Boolean, nullable=False, default=False)
-    is_legacy = db.Column(db.Boolean, nullable=False, default=False, index=True)
     # These fields are specifically for Project Phoenix
     designer_name = db.Column(db.String(100), nullable=True)
     designer_url = db.Column(db.String(255), nullable=True)
+
+    def __init__(self, name: str, stub: str = None):
+        if not stub:
+            stub = stubify(name)
+        self.name = name
+        self.stub = stub
 
 
 class UserRelease(db.AlchemyBase):
--- api/utils/helpers.py
+++ api/utils/helpers.py
@@ -5,4 +5,4 @@ def stubify(text: str) -> str:
     """Converts the given text into a stub"""
     if not text:
         return None
-    return re.sub(r"[^a-z0-9-]", "", text.trim().lower().replace(" ", "-"), flags=re.I)
+    return re.sub(r"[^a-z0-9-]", "", text.strip().lower().replace(" ", "-"), flags=re.I)
