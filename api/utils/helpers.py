47478f89e67bab6d2068f23a36f3f3aa31682095
Ian Beck <ian@onecrayon.com>
2020-10-04 11:23:34 -0700

    Added test and slightly improved stubify logic

--- api/utils/helpers.py
+++ api/utils/helpers.py
@@ -3,6 +3,7 @@ import re
 
 def stubify(text: str) -> str:
     """Converts the given text into a stub"""
+    text = text.strip()
     if not text:
         return None
-    return re.sub(r"[^a-z0-9-]", "", text.strip().lower().replace(" ", "-"), flags=re.I)
+    return re.sub(r"[^a-z0-9-]", "", text.lower().replace(" ", "-"), flags=re.I)
