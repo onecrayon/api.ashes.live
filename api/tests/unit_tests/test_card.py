ba3b8475a3e8de7bf2c0b9193e6ed8f6d9d55f73
Ian Beck <ian@onecrayon.com>
2020-10-04 11:09:46 -0700

    Added unit test for parsing cost weight

--- api/tests/unit_tests/test_card.py
+++ api/tests/unit_tests/test_card.py
@@ -1,4 +1,5 @@
 from api.models.card import Card, DiceFlags
+from api.services.card import parse_cost_to_weight
 
 
 def test_dice_to_flags():
@@ -31,3 +32,24 @@ def test_flags_to_dice():
     assert Card.flags_to_dice(
         DiceFlags.illusion.value + DiceFlags.divine.value + DiceFlags.time.value
     ) == [DiceFlags.illusion.name, DiceFlags.divine.name, DiceFlags.time.name]
+
+
+def test_card_weights():
+    """Card weights must be calculated properly"""
+    # Nonsense is 0
+    assert parse_cost_to_weight("1 ceremonial") == 0
+    # Nonsense that looks real is 0
+    assert parse_cost_to_weight("1 [[Biter]]") == 0
+    # Main weight is 5
+    assert parse_cost_to_weight("[[main]]") == 5
+    # Side weight is 4
+    assert parse_cost_to_weight("[[side]]") == 4
+    # Discard weight is 3 * cards discarded
+    assert parse_cost_to_weight("1 [[discard]]") == 3
+    assert parse_cost_to_weight("2 [[discard]]") == 6
+    # Basic die is 100
+    assert parse_cost_to_weight("1 [[basic]]") == 100
+    # Class die is 101
+    assert parse_cost_to_weight("1 [[ceremonial:class]]") == 101
+    # Power die is 102
+    assert parse_cost_to_weight("1 [[ceremonial:power]]") == 102
