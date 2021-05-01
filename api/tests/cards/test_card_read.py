324915fe39d5b684e3fd9d7808184e922aa6966a
Ian Beck <ian@onecrayon.com>
2021-05-01 16:32:30 -0700

    Added tests for reading card details
    Closes #12.

--- api/tests/cards/test_card_read.py
+++ api/tests/cards/test_card_read.py
@@ -29,7 +29,39 @@ def test_get_card(client: TestClient, session: db.Session):
     assert "is_legacy" not in response.json(), response.json()
 
 
-def test_get_nonexistant_card(client: TestClient, session: db.Session):
+def test_get_nonexistent_card(client: TestClient, session: db.Session):
     """Must throw an error when the card doesn't exist"""
     response = client.get("/v2/cards/no-luck")
     assert response.status_code == status.HTTP_404_NOT_FOUND, response.json()
+
+
+def test_get_details_no_card(client: TestClient, session: db.Session):
+    """Must fail properly when requesting non-existent card"""
+    response = client.get("/v2/cards/no-luck/details")
+    assert response.status_code == status.HTTP_404_NOT_FOUND, response.json()
+
+
+def test_get_details_root_phoenixborn(client: TestClient, session: db.Session):
+    """Must properly find root Phoenixborn cards"""
+    response = client.get("/v2/cards/example-conjured-alteration/details")
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_get_details_root_non_phoenixborn(client: TestClient, session: db.Session):
+    """Must properly find root non-Phoenixborn cards"""
+    response = client.get("/v2/cards/summon-example-conjuration/details")
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_get_details_root_phoenixborn_conjurations(
+    client: TestClient, session: db.Session
+):
+    """Must properly find conjurations when looking up Phoenixborn unique"""
+    response = client.get("/v2/cards/example-ally/details")
+    assert response.status_code == status.HTTP_200_OK
+
+
+def test_get_details_phoenixborn(client: TestClient, session: db.Session):
+    """Must properly find connected cards when looking up Phoenixborn"""
+    response = client.get("/v2/cards/example-phoenixborn/details")
+    assert response.status_code == status.HTTP_200_OK
